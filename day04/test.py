aa2fdc5fd748c50b9c32f380c7d6397081faf5cb
liangzhao <liang_zhao02@163.com>
2024-09-25 18:40:17 +0800

    update

---
+++ day04/test.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from time import sleep
+driver = webdriver.Chrome()
+driver.get('http://192.168.1.21:9999/')
+# 登录失败
+driver.find_element_by_link_text('登录').click()
+# 输入账号
+driver.find_element_by_id('mobile').send_keys('15296797153')
+# 输入密码
+driver.find_element_by_id('password').send_keys('1234')
+# 点击登录
+driver.find_element_by_css_selector('[value="登 录"]').click()
+# 切换警告框
+sleep(3)
+alert = driver.switch_to.alert
+# 判断
+assert alert.text == '用户名或者密码错误'
+sleep(3)
+driver.quit()
